0a204f87b6e0797d00f4b56c9baa5eb46404dcb2
ayushsonone07 <90081700+ayushsonone07@users.noreply.github.com>
2024-07-17 09:57:51 +0000

    added home page

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function profile({ params }: { params: { id: string } }) {
   return (
     <main className="flex w-screen h-screen bg-white py-2 gap-3 md:gap-40 justify-center">
-      <h1 className="text-black text-4xl font-bold">Hi, {params.id}</h1>
+      <h1 className="text-black text-3xl font-bold">Hi, <span className="font-semibold text-violet-700">{params.id}</span></h1>
     </main>
   );
 }
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Signin() {
   return (
-    <main className="flex w-screen h-screen min-h-screen flex-col items-center justify-between p-24 bg-gray-300">
+    <main className="flex w-screen h-screen min-h-screen flex-col items-center justify-between p-24 bg-gray-200">
       <div className="grid mx-auto my-auto col grid-cols-12 gap-3 rounded-lg z-0 shadow-2xl backdrop-blur-sm w-auto">
         <input
           type="text"
