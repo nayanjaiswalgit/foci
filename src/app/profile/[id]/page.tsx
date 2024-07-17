90a2b5df9a95f9a73ace1fc8f81f4174222cb1b8
ayushsonone07 <90081700+ayushsonone07@users.noreply.github.com>
2024-07-17 05:35:02 +0000

    general fixes

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function profile({ params }: { params: { slug: string } }) {
+export default function profile({ params }: { params: { id: string } }) {
   return (
     <main className="flex w-screen h-screen bg-white py-2 gap-3 md:gap-40 justify-center">
-      <h1 className="text-black text-4xl font-bold">Hi, {params.slug}</h1>
+      <h1 className="text-black text-4xl font-bold">Hi, {params.id}</h1>
     </main>
   );
 }
