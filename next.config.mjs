8e974b7c9aa1a50c2b3824fed82b812b07047c3a
ayushsonone07 <90081700+ayushsonone07@users.noreply.github.com>
2024-08-08 05:49:25 +0000

    some changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: "/api/webhook", // Adjust the path as needed
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://cuddly-carnival-pq9w647599vhgpv-3000.app.github.dev",
-          },
-        ],
-      },
-    ];
-  },
-};
+// module.exports = {
+//   async headers() {
+//     return [
+//       {
+//         source: "/api/webhook", // Adjust the path as needed
+//         headers: [
+//           {
+//             key: "Access-Control-Allow-Origin",
+//             value: "https://verbose-spork-qg64xjv5gpxc97xj-3000.app.github.dev",
+//           },
+//         ],
+//       },
+//     ];
+//   },
+// };
 
-export default nextConfig;
+// export default nextConfig;
